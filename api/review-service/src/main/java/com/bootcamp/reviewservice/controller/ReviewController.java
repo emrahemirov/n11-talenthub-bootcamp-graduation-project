e080fa155700b898a42ee4d9c3d401408883a68e
emrahemirov <103997751+emrahemirov@users.noreply.github.com>
2024-03-06 22:35:57 +0300

    feat: add services

---
+++ api/review-service/src/main/java/com/bootcamp/reviewservice/controller/ReviewController.java
@@ -0,0 +1,15 @@
+package com.bootcamp.reviewservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
